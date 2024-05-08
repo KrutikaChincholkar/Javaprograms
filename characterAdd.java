c29049feb52a3bd96a839eb497ed30d35a6ac4e2
Krutika Chincholkar <118680316+KrutikaChincholkar@users.noreply.github.com>
2024-05-08 22:27:28 +0530

    Create characterAdd.java
    use the + character to add a variable to another variable by two ways

---
+++ characterAdd.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    String firstName = "John ";
+    String lastName = "Doe";
+   String fullName = firstName + lastName;
+   System.out.println(fullName);  // one way 
+    System.out.println(firstName + lastName); // 2nd way   
+  }
+}
